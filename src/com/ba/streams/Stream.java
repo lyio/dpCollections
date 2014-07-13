76e6214959dc5b5950dbef77fbb38661f64f173e
Thomas Fiedler <thfied+github@gmail.com>
2014-07-13 20:45:45 +0200

    streams with filter and limit

--- src/com/ba/streams/Stream.java
+++ src/com/ba/streams/Stream.java
@@ -3,12 +3,25 @@ package com.ba.streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Stream {
 
     public static void main(final String[] args) {
-        final List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
+        final List<Integer> demoValues = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        ints.forEach(System.out::println);
+        //limit the input -> [1, 2, 3, 4]
+        System.out.println(demoValues
+                .stream()
+                .limit(4)
+                .collect(Collectors.toList()));
+
+        //limit the result -> [5, 6, 7, 8]
+        System.out.println(demoValues
+                .stream()
+                .filter((v) -> v > 4)
+                .limit(4)
+                .collect(Collectors.toList()));
+
     }
 }
