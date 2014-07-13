bf6f27161bc2c011201e16c616b86670eeed0d56
Thomas Fiedler <thfied+github@gmail.com>
2014-07-13 15:25:50 +0200

    forEach - ugly

--- src/com/ba/streams/Stream.java
+++ src/com/ba/streams/Stream.java
@@ -2,20 +2,18 @@ package com.ba.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Stream {
 
     public static void main(final String[] args) {
         final List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
 
-        for (int i = 0; i < ints.size(); i++) {
-            System.out.println(ints.get(i));
-        }
-
-        for (final int i : ints) {
-            if (ints.get(i) % 2 == 0) {
-                System.out.println(ints.get(i));
+        ints.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(final Integer v) {
+                System.out.println(v);
             }
-        }
+        });
     }
 }
