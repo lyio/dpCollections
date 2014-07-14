11c3564dd27c48131c854983dc075f0dea6dfd0a
Thomas Fiedler <t.fiedler@codecraft.de>
2014-07-14 09:31:46 +0200

    infinite stream of random numbers

--- src/com/ba/streams/Stream.java
+++ src/com/ba/streams/Stream.java
@@ -1,26 +1,13 @@
 package com.ba.streams;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 public class Stream {
 
     public static void main(final String[] args) {
-        final List<Integer> demoValues = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        //limit the input -> [1, 2, 3, 4]
-        System.out.println(demoValues
-                .stream()
-                .limit(4)
-                .collect(Collectors.toList()));
-
-        //limit the result -> [5, 6, 7, 8]
-        System.out.println(demoValues
-                .stream()
-                .filter((v) -> v > 4)
-                .limit(4)
-                .collect(Collectors.toList()));
+        java.util.stream.Stream.generate(() -> new Random().nextInt(100))
+                .filter(i -> i % 2 == 0)
+                .map(i -> i * i)
+                .forEach(i -> System.out.println(i));
     }
 }
